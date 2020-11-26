479c3d238a03dce1cda2d94430829b9f886fc6ad
Minh Tri Ho <b3r0ngheroq@gmail.com>
2020-11-26 07:54:41 +0700

    Sửa file lúc 8h sáng 26/11 bởi Minh Trí Hồ

--- fornangcao.py
+++ fornangcao.py
@@ -3,4 +3,9 @@ list2 = [({'name': 'Peter', 'age':2}, {'name': 'John', 'age':21}),
          ({'name': 'Nam', 'age':2}, {'name': 'Hung', 'age':21}),
          ({'name': 'Mai', 'age':2}, {'name': 'Loan', 'age':21})]
 for index, (x,y) in enumerate(list2):
-    print(x['name'], x['age'],'/n',y['name'],y['age'])
+    for i in x:
+        print(x[i],end=' ')
+    print(' ')
+    for i in y:
+        print(y[i],end=' ')
+    print(' ')
