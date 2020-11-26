c053cf78c0d1315214c753a0a391cc0881ad0958
eggtgg <73218635+eggtgg@users.noreply.github.com>
2020-11-26 08:19:40 +0700

    lần cuối 8h19 26/11/2020

--- fornangcao.py
+++ fornangcao.py
@@ -4,10 +4,10 @@ list2 = [({'name': 'Peter', 'age':2}, {'name': 'John', 'age':21}),
          ({'name': 'Mai', 'age':2}, {'name': 'Loan', 'age':21})]
 for index, (x,y) in enumerate(list2):
     #khi phân tích ra thì x, y là các dictionary
-    for i in x: #i chạy trong x nhận các giá trị lần lượt là: 'name', 'egg'
+    for i in x: #i chạy trong x nhận các giá trị lần lượt là: 'name', 'age'
         print(x[i],end=' ')
     print(' ')
-    for i in y: #i chạy trong y nhận các giá trị lần lượt là: 'name', 'egg'
+    for i in y: #i chạy trong y nhận các giá trị lần lượt là: 'name', 'age'
         print(y[i],end=' ')
     print(' ')
 '''khi chạy 2 vòng for như vậy thấy khá mất thời gian
